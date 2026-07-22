3b38957a7b558d500894d0479ee13d94db0cd4d5
hifzaabid458-cloud <hifzaabid458@gmail.com>
2026-07-22 16:58:37 +0500

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("127.0.0.1", "root", "", "abc-bank");
-
-if ($conn->connect_error) {
-    die("Connection Failed: " . $conn->connect_error);
-}
-
-echo "✅ Database Connected Successfully!";
-?>
